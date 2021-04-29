a89b6a3040f8da66f3c8bb9352f75917fb38dff6
Alain Picard <devcoder0x0@gmail.com>
2021-04-28 17:23:32 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,27 @@
+//JavaScript Functions, Scoping, Program Flow//
+
+
+/*Exercise #6
+    //  Write a function that takes an Array of strings as an argument and prints the first letter of each element out (one per line). 
+    //  For example the Array ["Hello", "World", "This", "Is", "My", "String"] should get printed as follows:
+    
+    H
+    W
+    T
+    I
+    M
+    S
+    
+*/ 
+//  Resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+//             https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+
+//  Write your code below:
+//  Use console.log() to check your results in the browser
+const arr = ["Hello", "World", "This", "Is", "My", "String"];
+
+
+
+
+
+
